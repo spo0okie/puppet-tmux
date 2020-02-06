99480d3a4308e603f541a6e215e525cfd9006117
Alexander Reviakin <reviakin@gmail.com>
2020-02-06 23:44:50 +0500

    centos8 fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,27 @@
+#класс установки и настройки тмукса
 class tmux {
-	require repos::ius
-	exec {'tmux1_remove':
-		command => 'yum remove tmux -y',
-		onlyif  => 'rpm -qa |grep tmux && tmux -V |grep "tmux 1."',
-		path    => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin'
-	} ->
-	package {'tmux2':
-		ensure => installed,
-	} ->
+	case $::operatingsystem {
+		'CentOS': {
+			case $::operatingsystemmajrelease {
+				'8': {
+					package {'tmux':
+						ensure => installed,
+					} ->
+				}
+				default: {
+					require repos::ius
+					exec {'tmux1_remove':
+						command => 'yum remove tmux -y',
+						onlyif  => 'rpm -qa |grep tmux && tmux -V |grep "tmux 1."',
+						path    => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin'
+					} ->
+					package {'tmux2':
+						ensure => installed,
+					} ->
+				}
+			}
+		}
+	}
 	file {'/root/.tmux.conf':
 		source => 'puppet:///modules/tmux/.tmux.conf',
 		owner => 'root',
